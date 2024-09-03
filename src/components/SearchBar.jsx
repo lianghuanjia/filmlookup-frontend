1ddce0a61ccbc9fc656b7ddddf5c3bfabf3328a7
Huanjia Liang <huanjial@uci.edu>
2024-09-02 22:42:13 -0700

    Change using MUI's TextField to using my own input field. Customized the width.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { TextField, IconButton } from '@mui/material';
+import { IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import '../App.css';
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState('');
@@ -14,21 +15,37 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div>
-      <TextField
-        variant="outlined"
+    <div style={{ width: '100%', maxWidth: '60vw', margin: '0 auto', position: 'relative' }}>
+      <input
+        type="text"
         value={query}
         onChange={handleInputChange}
         placeholder="Search for movies..."
-        InputProps={{
-          endAdornment: (
-            <IconButton onClick={handleSearch}>
-              <SearchIcon />
-            </IconButton>
-          ),
+        className="custom-movie-search-text"
+        style={{
+          width: '50vw',
+          height: '30px',
+          padding: '10px 50px 10px 20px', 
+          fontSize: '19px',
+          border: '1px solid #ccc',
+          borderRadius: '30px', // Rounded corners
+          outline: 'none',
         }}
-        fullWidth
       />
+      <IconButton
+        onClick={handleSearch}
+        style={{
+          position: 'absolute',
+          right: '0px', // Align to the right edge of the input
+          top: '0px',
+          height: '100%', // Make sure the button spans the full height of the input
+          padding: '0px 10px',
+          borderRadius: '0 16px 16px 0', // Rounded right corners
+          color: 'grey',
+        }}
+      >
+        <SearchIcon />
+      </IconButton>
     </div>
   );
 };
