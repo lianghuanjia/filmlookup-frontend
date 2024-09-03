7a7c40290c47913c0067bf2e0dde12f1dec3e262
Huanjia Liang <huanjial@uci.edu>
2024-09-03 09:02:58 -0700

    2 of N: direct to movie detail page: Modify the SearchResults.jsx so that clicking a movie block can go to the MovieDetail.jsx page

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../App.css'; // Import the CSS file
 
 const SearchResults = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const movies = location.state?.movies || [];
 
   const generateRandomDate = () => {
@@ -33,13 +34,22 @@ const SearchResults = () => {
     return formatDateToHumanReadable(new Date(releaseTime));
   };
 
+  const handleMovieClick = (id) => {
+    navigate(`/movie/${id}`); // Navigate to the MovieDetail page with the movie ID
+  };
+
   return (
     <div>
       <h1>Search Results</h1>
       {movies.length > 0 ? (
         <div className="results-container">
           {movies.map((movie) => (
-            <div key={movie.id} className="movie-block">
+            <div 
+              key={movie.id} 
+              className="movie-block"
+              onClick={() => handleMovieClick(movie.id)} // Handle click event
+              style={{ cursor: 'pointer' }}
+              >
               <img
                 src={`https://image.tmdb.org/t/p/w200${movie.posterPath}`}
                 alt={`${movie.title} poster`}
