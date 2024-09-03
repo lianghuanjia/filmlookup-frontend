b72544755933ab838d3c4bd375c65276f55a5563
Huanjia Liang <huanjial@uci.edu>
2024-09-03 08:53:57 -0700

    1 of N: Direct to movie detail page: Create a MovieDetail.jsx for movie detail page, add a route in the App.jsx so that it can route users to the movie detail page.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MovieSearch from './pages/MovieSearch';
 import SearchResults from './pages/SearchResults';
+import MovieDetail from './pages/MovieDetail';
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MovieSearch />} />
         <Route path="/results" element={<SearchResults />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieDetail = () => {
+  return (
+    <div>
+      <h1>New page</h1>
+    </div>
+  );
+};
+
+export default MovieDetail;
